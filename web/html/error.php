f5d20641fae7d21b08d2ad7004d6fc0469f0f674
Björn Mattsson <bjorn@sunet.se>
2024-05-21 15:28:20 +0200

    Added statusMessage

--- web/html/error.php
+++ web/html/error.php
@@ -52,6 +52,8 @@ function showFatalProfileException() {?>
       "\n          <p>statusCode : " . htmlspecialchars($_GET['statusCode']) . '</p>' : '';
     print isset($_GET['statusCode2']) ?
       "\n          <p>statusCode2 : " . htmlspecialchars($_GET['statusCode2']) . '</p>' : '';
+    print isset($_GET['statusMessage']) ?
+      "\n          <p>statusMessage : " . htmlspecialchars($_GET['statusMessage']) . '</p>' : '';
     print "\n        </div>\n";
  }
 
